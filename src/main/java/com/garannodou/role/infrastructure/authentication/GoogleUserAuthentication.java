98ecc2d7de0a948f5c09c43a3d4432cb79a910c1
mduenast <ratchetmdt@gmail.com>
2020-08-15 11:49:03 +0200

    Add getters.

--- src/main/java/com/garannodou/role/infrastructure/authentication/GoogleUserAuthentication.java
+++ src/main/java/com/garannodou/role/infrastructure/authentication/GoogleUserAuthentication.java
@@ -1,17 +1,27 @@
 package com.garannodou.role.infrastructure.authentication;
 
+import lombok.Getter;
+
 import java.util.UUID;
 
 import static java.util.UUID.randomUUID;
 
 public class GoogleUserAuthentication {
+    @Getter
     private UUID id;
+    @Getter
     private UUID userId;
+    @Getter
     private String accessToken;
+    @Getter
     private String idToken;
+    @Getter
     private String scope;
+    @Getter
     private long expiresIn;
+    @Getter
     private long firstIssuedAt;
+    @Getter
     private long expiresAt;
 
     public GoogleUserAuthentication(UUID userId, String accessToken, String idToken, String scope, long expiresIn,
