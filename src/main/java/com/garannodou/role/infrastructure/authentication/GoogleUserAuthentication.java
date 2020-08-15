f8b21fb1f043ddd87feac675e454a28776eb66a4
mduenast <ratchetmdt@gmail.com>
2020-08-15 11:56:28 +0200

    Add tests for User.

--- src/main/java/com/garannodou/role/infrastructure/authentication/GoogleUserAuthentication.java
+++ src/main/java/com/garannodou/role/infrastructure/authentication/GoogleUserAuthentication.java
@@ -7,21 +7,13 @@ import java.util.UUID;
 import static java.util.UUID.randomUUID;
 
 public class GoogleUserAuthentication {
-    @Getter
     private UUID id;
-    @Getter
     private UUID userId;
-    @Getter
     private String accessToken;
-    @Getter
     private String idToken;
-    @Getter
     private String scope;
-    @Getter
     private long expiresIn;
-    @Getter
     private long firstIssuedAt;
-    @Getter
     private long expiresAt;
 
     public GoogleUserAuthentication(UUID userId, String accessToken, String idToken, String scope, long expiresIn,
