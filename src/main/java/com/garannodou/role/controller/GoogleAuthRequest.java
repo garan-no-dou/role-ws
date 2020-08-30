f7b715fb026a444d1f4f8a2ba9547d31dbbddf11
Roger Arqués <rarquesvall@gmail.com>
2020-08-30 12:58:45 +0200

    Applied PR comments and removed power mockito.

--- src/main/java/com/garannodou/role/controller/GoogleAuthRequest.java
+++ src/main/java/com/garannodou/role/controller/GoogleAuthRequest.java
@@ -10,23 +10,27 @@ import javax.validation.constraints.NotNull;
 @Setter
 public class GoogleAuthRequest {
 
-    @NotNull
-    private GoogleBasicProfile googleBasicProfile;
-    @NotNull
-    private GoogleAuthResponse googleAuthResponse;
+    private GoogleBasicProfile googleBasicProfile = new GoogleBasicProfile();
+    private GoogleAuth googleAuth = new GoogleAuth();
 
     public GoogleAuthRequest() {
     }
 
     @Getter
     @Setter
-    static class GoogleBasicProfile {
-        @NotNull private String id;
-        @NotNull private String name;
-        @NotNull private String givenName;
-        @NotNull private String familyName;
-        @NotNull private String imageUrl;
-        @NotNull private String email;
+    class GoogleBasicProfile {
+        @NotNull
+        private String id;
+        @NotNull
+        private String name;
+        @NotNull
+        private String givenName;
+        @NotNull
+        private String familyName;
+        @NotNull
+        private String imageUrl;
+        @NotNull
+        private String email;
 
         public GoogleBasicProfile() {
         }
@@ -34,15 +38,21 @@ public class GoogleAuthRequest {
 
     @Getter
     @Setter
-    static class GoogleAuthResponse {
-        @NotNull private String accessToken;
-        @NotNull private String idToken;
-        @NotNull private String scope;
-        @NotNull private String expiresIn;
-        @NotNull private String firstIssuedAt;
-        @NotNull private String expiresAt;
+    class GoogleAuth {
+        @NotNull
+        private String accessToken;
+        @NotNull
+        private String idToken;
+        @NotNull
+        private String scope;
+        @NotNull
+        private String expiresIn;
+        @NotNull
+        private String firstIssuedAt;
+        @NotNull
+        private String expiresAt;
 
-        public GoogleAuthResponse() {
+        public GoogleAuth() {
         }
     }
 
