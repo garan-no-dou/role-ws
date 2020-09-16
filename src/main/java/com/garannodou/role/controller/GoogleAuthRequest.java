6ef8b1bb4feddd3fa8a5f3274c5478987c3d4f62
mduenast <ratchetmdt@gmail.com>
2020-09-16 22:21:16 +0200

    Add length limitation annotations.

--- src/main/java/com/garannodou/role/controller/GoogleAuthRequest.java
+++ src/main/java/com/garannodou/role/controller/GoogleAuthRequest.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -26,21 +27,26 @@ public class GoogleAuthRequest {
         @NotNull
         @Pattern(regexp = "^[a-zA-Z0-9-_\\s]+$",
                 message = "Name has invalid format. Expected format: ^[a-zA-Z0-9-_\\s]+$")
+        @Size(min = 10, max = 50)
         private String name;
         @NotNull
         @Pattern(regexp = "^[a-zA-Z0-9-_\\s]+$",
                 message = "GivenName has invalid format. Expected format: ^[a-zA-Z0-9-_\\s]+$")
+        @Size(min = 10, max = 50)
         private String givenName;
         @NotNull
         @Pattern(regexp = "^[a-zA-Z0-9-_\\s]+$",
                 message = "FamilyName has invalid format. Expected format: ^[a-zA-Z0-9-_\\s]+$")
+        @Size(min = 10, max = 100)
         private String familyName;
         @NotNull
         @URL
+        @Size(min = 10, max = 100)
         private String imageUrl;
         @NotNull
         @Email(regexp = "^[A-Za-z0-9+_.-]+@([A-Za-z0-9.])+$",
                 message = "Email has invalid format. Expected format: ^[A-Za-z0-9+_.-]+@(.+)$")
+        @Size(min = 10, max = 100)
         private String email;
 
         public GoogleBasicProfile() {
@@ -52,16 +58,22 @@ public class GoogleAuthRequest {
     class GoogleAuth {
         // TODO: Add validations to each field, once the content is clearer.
         @NotNull
+        @Size(min = 10, max = 100)
         private String accessToken;
         @NotNull
+        @Size(min = 10, max = 100)
         private String idToken;
         @NotNull
+        @Size(min = 10, max = 100)
         private String scope;
         @NotNull
+        @Size(min = 10, max = 100)
         private String expiresIn;
         @NotNull
+        @Size(min = 10, max = 100)
         private String firstIssuedAt;
         @NotNull
+        @Size(min = 10, max = 100)
         private String expiresAt;
 
         public GoogleAuth() {
