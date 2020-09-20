fd279d251b4c839eaa218b6322c96d34ae86d242
Roger Arqués <rarquesvall@gmail.com>
2020-09-20 12:48:40 +0200

    Implemented use cases in UserService.

--- src/main/java/com/garannodou/role/RoleWsApplication.java
+++ src/main/java/com/garannodou/role/RoleWsApplication.java
@@ -1,13 +1,33 @@
 package com.garannodou.role;
 
+import com.garannodou.role.domain.User;
+import com.garannodou.role.domain.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class RoleWsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RoleWsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RoleWsApplication.class, args);
+    }
+
+    @Bean // TODO: Remove when UserRepository is implemented
+    public UserRepository userRepository() {
+        return new UserRepository() {
+            @Override
+            public Optional<User> findByEmail(String email) {
+                return Optional.empty();
+            }
+
+            @Override
+            public User save(User user) {
+                return user;
+            }
+        };
+    }
 
 }
